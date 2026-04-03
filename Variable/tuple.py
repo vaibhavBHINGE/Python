598914f5786577c4c04b0c3059645e9d46d16140
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-04-03 11:48:42 +0530

    Tuple

---
+++ Variable/tuple.py
@@ -0,0 +1,6 @@
+# Tuples are immutalbe
+tup=(12,13,141,2,3,32,23,23,3,3,3,3,3,3,33,516,1671,31,3113,432,3,4243,5345345)
+print(tup)
+print("feting values using index at 4: ",tup[4])
+print("count of the value 3: ",tup.count(3))
+print("index in tuple written a index of given value 141: ",tup.index(141))
