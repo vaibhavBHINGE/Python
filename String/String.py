2be50b41618fa586f995c530195d92693ec66ff6
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-04-05 08:48:58 +0530

    String example with its methods

---
+++ String/String.py
@@ -0,0 +1,7 @@
+Name="Vaibhav"
+print(Name)
+print("vaibhav ""Bhinge")
+print("fetching the value form the 3 index: ",Name[3])
+print("fetching the last value using -1 index: ",Name[-1])
+print("fetching the value form the 3 an 6 index: ",Name[3:7])
+print("lenght of the given string variable: ",len(Name))
