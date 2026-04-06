93de4a07303751cac89b682f22861c041de690c0
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-04-06 23:39:28 +0530

    if else

---
+++ conditional statements/if-else.py
@@ -0,0 +1,29 @@
+x=11;
+if(x%2==0):
+    print("given number is even: ",x)
+    if(x%4==0):
+        print("this number is divided by 4: ",x)
+    else:
+        print("number is not divide by 4: ",x)
+else:
+    print("given number is odd: ",x)
+
+    
+y=7
+if(y==1):
+    print("one")
+
+elif(y==2):
+    print("two")
+
+elif(y==3):
+    print("three")
+    
+elif(y==4):
+ print("three")
+
+elif(y==5):
+ print("five")
+
+else:
+   print("enter number between 1 & 5")
